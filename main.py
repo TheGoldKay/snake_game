b3bdd3df64b571a749d7212f5255887b4f6aeee3
ruby <jhonnatan.flr@gmail.com>
2022-07-06 03:57:54 -0300

    grid & boxes

--- main.py
+++ main.py
@@ -1,16 +1,54 @@
 import pygame
 
+WIN_WIDTH = 500
+WIN_HEIGHT = 500
+BG_COLOR = (20, 92, 68)
+WHITE = (255, 255, 255)
+
+class Box:
+  def __init__(self, row, column, height, width):
+    self.r = row 
+    self.c = column 
+    self.h = height
+    self.w = width
+  
+  def draw(self, surf):
+    rect = pygame.Rect(self.c * self.w, self.r * self.h, self.w, self.h)
+    pygame.draw.rect(surf, (0,0,0), rect, 1)
+
+
+
+def make_grid(rows, cols):
+  width = WIN_WIDTH / cols 
+  height = WIN_HEIGHT / rows 
+  boxlist = []
+  for r in range(rows):
+    column = []
+    for c in range(cols):
+      box = Box(r, c, height, width)
+      column.append(box)
+    boxlist.append(column)
+  return boxlist
+
+def draw_grid(grid, surf):
+  for line in grid:
+    for box in line:
+      box.draw(surf)
+
 def main():
-  background_colour = (20, 92, 68)
-  screen = pygame.display.set_mode((600, 400))
+  screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
   pygame.display.set_caption('Snake')
-  screen.fill(background_colour)
+  screen.fill(BG_COLOR)
   pygame.display.flip()
+  grid = make_grid(25, 25)
   running = True
   while running:
+    screen.fill(BG_COLOR)
     for event in pygame.event.get(): 
       if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
         running = False
+    draw_grid(grid, screen)
+    pygame.display.flip()
   pygame.quit()
 
 if __name__ == '__main__':
