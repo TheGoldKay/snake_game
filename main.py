24cdcf291fe48628dc9925a8b4013ba0e9861a08
ruby <jhonnatanwfc@live.com>
2022-07-14 17:05:40 -0300

    food

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import time
+import random
 
 FPS = 60
 WIN_WIDTH = 620
@@ -10,24 +11,38 @@ shift = 10
 WHITE = (BG_COLOR[0]+shift, BG_COLOR[1]+shift, BG_COLOR[2]+shift)
 shift = 35
 SNAKE_COLOR = (BG_COLOR[0]+shift, BG_COLOR[1]+shift, BG_COLOR[2]+shift)
+FOOD_COLOR = (255, 0, 0)
 
 class Box:
-  def __init__(self, row, column, size, color = WHITE, width = 1):
+  def __init__(self, row, column, size = BOX_SIZE, color = WHITE, width = 1):
     self.r = row 
     self.c = column 
     self.s = size 
     self.color = color 
     self.width = width 
   
-  def draw(self, surf):
-    rect = pygame.Rect(self.c * self.s, self.r * self.s, self.s, self.s)
-    pygame.draw.rect(surf, self.color, rect, self.width)
+  def _get_rect(self):
+    return pygame.Rect(self.c * self.s, self.r * self.s, self.s, self.s)
 
+  def draw(self, surf):
+    pygame.draw.rect(surf, self.color, self._get_rect(), self.width)
+
+class Food(Box):
+  def __init__(self):
+    row, column = self._get_rand_pos()
+    super().__init__(row, column, BOX_SIZE, FOOD_COLOR, 0)
+
+  def _get_rand_pos(self):
+    return random.randint(1, WIN_HEIGHT // BOX_SIZE), random.randint(1, WIN_WIDTH // BOX_SIZE)
+  
+  def hitbox(self, snake):
+    if self.r == snake[0][0] and self.c == snake[0][1]:
+       self.r, self.c = self._get_rand_pos()
 
 class Snake:
-  def __init__(self, head_row, head_col, size):
+  def __init__(self, head_row, head_col):
     self.b = [[head_row, head_col]]
-    self.s = size 
+    self.s = BOX_SIZE 
     self.timer = 0.2
     self.clock = time.time()
     self.vx = -1
@@ -39,6 +54,9 @@ class Snake:
     for box in self.b:
       rect = pygame.Rect(box[1] * self.s, box[0] * self.s, self.s, self.s)
       pygame.draw.rect(surf, SNAKE_COLOR, rect)  
+  
+  def get_body(self):
+    return self.b 
   
   def update(self):
     if time.time() - self.clock > self.timer:
@@ -94,7 +112,8 @@ def main():
   screen.fill(BG_COLOR)
   pygame.display.flip()
   grid = make_grid(BOX_SIZE)
-  snake = Snake(10, 10, BOX_SIZE)
+  snake = Snake(10, 10)
+  food = Food()
   clock = pygame.time.Clock()
   running = True
   while running:
@@ -107,7 +126,9 @@ def main():
         if event.key == pygame.K_ESCAPE:
           running = False 
         snake.move(event.key)
+    food.hitbox(snake.get_body())
     snake.update()
+    food.draw(screen)
     draw_grid(grid, screen)
     snake.draw(screen)
     pygame.display.flip()
